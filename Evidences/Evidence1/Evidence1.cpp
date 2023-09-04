ddca46c3feb47e6e90bb1c6521fc3922c62e5a8d
David A. Lozano <void9david@gmail.com>
2023-09-04 11:49:42 -0600

    evidence 1 start

---
+++ Evidences/Evidence1/Evidence1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+int main() {
+
+    // Archivo de entrada
+    ifstream fileIn("log603.txt");
+    // Archivo de salida
+    ofstream fileOut("output603.out");
+    // Variable auxiliar para guardar el contenido del renglón leido
+    string line;
+    // Variable auxiliar para guardar el contenido de cada palabra del campo message
+    string word;
+    // Variables auxiliares para guardar el contenido de cada campo del archivo
+    string year;
+    string month;
+    string day;
+    string time;
+    string ip;
+    string message;
+    // Variable auxiliar para guardar el contador de registros
+    int cont = 0;
+    // Ciclo para leer todo los renglones del archivo de entrada
+    while (getline(fileIn,line)) { 
+        // Creamos una variable auxiliar ss para recorrer campo por campo
+        stringstream ss(line);
+        // Guardamos la información de cada campo en la variable auxiliar correspondiente
+        ss >> month >> day >> year >> time >> ip >> message;
+        // Ciclo para recorrer cada palabra del campo message
+        while (ss >> word) {
+            message = message + " " + word;
+        }
+        cout << month << " " << day << " " << year << " " << time << " " << ip << " " << message << endl;
+        // Incrementamos el contador de registros
+        cont++;
+    }
+
+
+    return 0;
+}
