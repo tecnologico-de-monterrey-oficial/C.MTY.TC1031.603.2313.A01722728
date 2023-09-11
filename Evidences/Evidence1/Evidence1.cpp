f2631da20416986fdb06802bbbaf8c12dc182b1f
David A. Lozano <void9david@gmail.com>
2023-09-10 19:56:51 -0600

    ev 1 version 1.0

--- Evidences/Evidence1/Evidence1.cpp
+++ Evidences/Evidence1/Evidence1.cpp
@@ -13,6 +13,7 @@ int main() {
     ifstream fileIn("log603.txt");
     // Archivo de salida
     ofstream fileOut("output603.out");
+    ofstream rangeOut("range603.out");
     // Variable auxiliar para guardar el contenido del renglón leido
     string line;
     // Variable auxiliar para guardar el contenido de cada palabra del campo message
@@ -25,6 +26,12 @@ int main() {
     string ip;
     string message;
     vector<Log> logs;
+    // variables para fecha final
+    string year2;
+    string month2;
+    string day2;
+    string time2;
+    
     // Variable auxiliar para guardar el contador de registros
     int cont = 0;
     // Ciclo para leer todo los renglones del archivo de entrada
@@ -43,7 +50,6 @@ int main() {
         cont++;
     }
 
-    cout << "La cantidad de registros es: " << logs.size() << endl;
     // Quicksort para ordenar el vector logs
     quickSort(logs, 0, logs.size()-1);
     // Escribiendo los registros ordenados en el archivo de salida
@@ -52,6 +58,38 @@ int main() {
     }
 
 
+    cout << "La cantidad de registros es: " << logs.size() << endl;
+
+    // Asking for a range of dates
+    cout << "Using binary search to find dates between range: " << endl;
+    cout << "Initial date: " << endl;
+    cout << "Year: ";
+    cin >> year;
+    cout << "Month capitalized and in 3 letter format, (Jan, Feb, Mar): ";
+    cin >> month;
+    cout << "Day: ";
+    cin >> day;
+    cout << "Time in 24h format hh:mm:ss: ";
+    cin >> time;    
+    cout << "Final date: " << endl;
+    cout << "Year: ";
+    cin >> year2;
+    cout << "Month capitalized and in 3 letter format, (Jan, Feb, Mar): ";
+    cin >> month2;
+    cout << "Day: ";
+    cin >> day2;
+    cout << "Time in 24h format hh:mm:ss: ";
+    cin >> time2;
+
+    Log date1(year, month, day, time, "", "");
+    Log date2(year2, month2, day2, time2, "", "");
+
+
+    for (int i = 0; i < logs.size(); i++) {
+        if (date1 < logs[i] && logs[i] < date2) {
+            rangeOut << logs[i].year << " " << logs[i].month << " " << logs[i].day << " " << logs[i].time << " " << logs[i].ip << " " << logs[i].message << endl;
+        } 
+    }
     // Cerramos los archivos de entrada y salida
     fileOut.close(); 
     fileIn.close();
